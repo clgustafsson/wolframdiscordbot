e5dff0b79ac0fa7e2c2abbc0c7cfb373b288ac36
clgustafsson <58473037+clgustafsson@users.noreply.github.com>
2022-02-16 15:04:36 +0100

    Update bot.py

--- wolframbot/bot.py
+++ wolframbot/bot.py
@@ -1,6 +1,8 @@
 import discord
 import os
 import wolframalpha
+import io
+import aiohttp
 
 def bftranslator(bf):
     list = bf.split("(")
@@ -11,14 +13,27 @@ def bftranslator(bf):
     rf = "("+str(rft)+"/"+str(rfn)+")"
     return rf
 
+
+def findendpindex(start, string):
+    counter = 1
+    for endi in range(start, len(string)):
+        if (string[endi] == '('):
+            counter += 1
+        if (string[endi] == ')'):
+            counter -= 1
+            if counter == 0:
+                return endi
+    return -1
+
+
 print(bftranslator("4(1/2"))
 
-keys = open('../keys.txt', 'r').read()
+keys = open('keys.txt', 'r').read()
 keylist = keys.split(",")
 
 client = discord.Client()
-app_idwolfram = keylist[0]
-clientwolfram = wolframalpha.Client(app_idwolfram)
+app_id_wolfram = keylist[0]
+client_wolfram = wolframalpha.Client(app_id_wolfram)
  
 @client.event
 async def on_ready():
@@ -32,14 +47,15 @@ async def on_message(message):
    if message.content.startswith('!wolfram'):
        question = message.content[8:]   
        if "bf" in question:
-           #in work
+           
            bftranslator
        await message.channel.send("Question: " + str(question))
-       res = clientwolfram.query(question)
+       res = client_wolfram.query(question)
        answer = next(res.results).text
        await message.channel.send("Answer: " + str(answer))
+
 
    elif '!help' in message.content:
        await message.channel.send('use format !wolfram {question} in standard wolfram format')
 
-client.run(keylist[1])
+client.run(keylist[1]) 
